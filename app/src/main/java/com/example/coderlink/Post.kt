ea519cb715342513dfcf2375e7d0b77f71b41186
Janoff14 <sanjarqodirjanov@gmail.com>
2021-08-07 13:04:20 +0500

    first commit

---
+++ app/src/main/java/com/example/coderlink/Post.kt
@@ -0,0 +1,9 @@
+package com.example.coderlink
+
+data class Post(
+
+    val idToken: String,
+    val title: String,
+    val tags: List<String>,
+    val text: String
+)
---
+++ app/src/main/java/com/example/coderlink/User.kt
@@ -0,0 +1,11 @@
+package com.example.coderlink
+
+data class User(
+    val name: String,
+    val last_name: String,
+    val email: String,
+    val phone_number : String,
+    val user_id: String,
+    val work: String,
+    val education: String
+)
